442c5fcc666f0da4fd134dd806341b9b5540c76f
Todd <todd@aeliustech.com>
2025-11-16 08:27:17 -0500

    cleanup

--- TinyAVR-012/AVRAda_RTS/obj/ada_u32_img.d
+++
@@ -1,3 +0,0 @@
-ada_u32_img.o: /home/todd/src/panoptes5.a/tinyAVR-Ada/TinyAVR-012/AVRAda_RTS/asm/ada_u32_img.S 20251005145017 \
-/home/todd/src/panoptes5.a/tinyAVR-Ada/TinyAVR-012/AVRAda_RTS/asm/asmdef.h 20251005145017 \
-/home/todd/src/panoptes5.a/tinyAVR-Ada/TinyAVR-012/AVRAda_RTS/asm/sectionname.h 20251005145017
